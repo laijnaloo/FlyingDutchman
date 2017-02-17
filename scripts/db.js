634f9f97577a9d04e6519262d88b5c2767c07ee7
TobiasAnderssonq <anderssontobias93@gmail.com>
2017-02-17 18:05:48 +0100

    Uppdaterade skriptet till databas och fixade någon typ av login

--- scripts/db.js
+++ scripts/db.js
@@ -2,24 +2,23 @@
  * Created by Tobbe on 2017-02-15.
  */
 
-var DB_URL = "http://pub.jamaica-inn.net/fpdb/api.php";
+var DB_URL = "http://pub.jamaica-inn.net/fpdb/api.php"
 
-function db_action() { //Ska ta emot argument
-    var uname = "jorass";
-    var password = "jorass";
-    var action = "user_get_all";
+function db_action(uname, password, action, callback) {
 
     var xhr = new XMLHttpRequest();
-    var reqURL = DB_URL+"?username="+uname+"&password="+password+"&action="+action;
+    var reqURL = DB_URL+"?action="+action+"&username="+uname+"&password="+password;
     xhr.open("GET", reqURL, true);
+    xhr.send();
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var resp = JSON.parse(xhr.responseText);
             console.log(resp);
-            return resp;
+            callback(resp);
         }
-    };
-    xhr.send();
+    }
+
+
 }
 
---
+++ scripts/login.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Tobbe on 2017-02-17.
+ */
+
+var unameField;
+var pwField;
+var uname;
+var password;
+
+function init() {
+    unameField = document.getElementById("unameLoginOption");
+    pwField = document.getElementById("passwordLoginOption");
+}
+
+
+
+function login() {
+    sessionStorage.clear();
+    uname = unameField.value;
+    password = pwField.value;
+    db_action(uname, password,"iou_get",function(result) {
+        if (result.type == "iou_get") {
+            sessionStorage.setItem("User", uname);
+            sessionStorage.setItem("Firstname", result.payload[0].first_name);
+            sessionStorage.setItem("Lastname", result.payload[0].last_name);
+            sessionStorage.setItem("Assets", result.payload[0].assets);
+            location = "OrderPage.html";
+        } else {
+
+            alert("Invalid username or password"); //Göra ngt snyggt kanske
+        }
+
+    });
+
+}
+
+window.addEventListener("DOMContentLoaded", init, false);
