6faa009afeca798b1edf713e56017bb4085a1f8d
peberglund <peterberglund90@gmail.com>
2017-02-27 13:17:44 +0100

    working language support

--- scripts/language.js
+++ scripts/language.js
@@ -1,48 +1,32 @@
-/*
-*
-* KOMMENTAR 1: Denna fil är för att kunna ändra språk! Koden är mest för att testa strukturen, så den ska skrivas om
-* till lite bättre kod! Tanken är väl att texterna för sidan inte ska hårdkodas
-*
-* KOMMENTAR 2: Cookies har implementeras, så måste köras via MAMP för att det ska funka korrekt
-*
-* KOMMENTAR 3: Bug i hur flaggorna fadeas!!
-*
-* */
-
 $(document).ready(function(){
 
-    var lang = getCookie("language");
+    if(localStorage.getItem("language")==undefined) {
+        localStorage.setItem("language", "sv    ");
+    }
 
-    $("#welcome").text(translate("welcome-string",lang));
-    $("#sortiment").text(translate("sort",lang));
-    $("#login").text(translate("login",lang));
-    $("#staffOption").text(translate("staff",lang));
-    $("#logintext").text(translate("login",lang));
-    $("#back").text(translate("back",lang));
-    $("#log").text(translate("login",lang));
+    var lang = localStorage.getItem("language");
 
-    if (lang ="sv"){
+    writeData(lang);
+
+
+
+    if (lang =="en"){
         $("#sv").fadeTo("fast", 0.20);
         $("#en").fadeTo("fast", 1);
 
     }else{
-        $("#sv").fadeTo("fast", 0.20);
-        $("#en").fadeTo("fast", 1);
+        $("#sv").fadeTo("fast", 1);
+        $("#en").fadeTo("fast", 0.20);
     }
+
 
     $("#sv").click(function(){
         $("#en").fadeTo("fast", 0.20);
         $("#sv").fadeTo("fast", 1);
 
-        deleteCookie("language");
-        setCookie("language","sv",365);
-        $("#welcome").text(translate("welcome-string","sv"));
-        $("#sortiment").text(translate("sort","sv"));
-        $("#login").text(translate("login","sv"));
-        $("#staffOption").text(translate("staff","sv"));
-        $("#logintext").text(translate("login","sv"));
-        $("#back").text(translate("back","sv"));
-        $("#log").text(translate("login","sv"));
+        localStorage.setItem("language", "sv");
+        lang = localStorage.getItem("language");
+        writeData(lang);
 
     });
 
@@ -50,67 +34,48 @@ $(document).ready(function(){
         $("#sv").fadeTo("fast", 0.20);
         $("#en").fadeTo("fast", 1);
 
-        deleteCookie("language");
-        setCookie("language","en",365);
-
-        $("#welcome").text(translate("welcome-string","en"));
-        $("#sortiment").text(translate("sort","en"));
-        $("#login").text(translate("login","en"));
-        $("#staffOption").text(translate("staff","en"));
-        $("#logintext").text(translate("login","en"));
-        $("#back").text(translate("back","en"));
-        $("#log").text(translate("login","en"));
+        localStorage.setItem("language", "en");
+        lang = localStorage.getItem("language");
+        writeData(lang);
     });
 });
 
-function deleteCookie(name){
-    document.cookie = "name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires;
-}
+function getData(string,language,place) {
+    $.ajax({
+        url: "test.txt",
+        dataType: "text",
+        success: function (data) {
+            var d = data;
+            var options = d.split(";");
+            var i=0;
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
+            for (i;i<=options.length;i++){
+                var option = [];
+                option = options[i].split(',');
+                if (option[0] == string){
+                    if (language == "en")
+                    {
+                        $(place).html(option[2]);
 
-function checkCookie() {
-    var lang = getCookie("language");
-    if (lang == "") {
-         setCookie("language", "sv", 365);
-        $("#en").fadeTo("fast", 0.20);
-    }
-}
+                    } else{
+                        $(place).text(option[1]);
 
-function translate(string, language){
-
-    operation = ["welcome-string","sort","login","staff","back"];
-    swedish = ["Välkommen","Sortiment","Logga in","Personal","Tillbaka"];
-    english = ["Welcome","Collections","Login","Staff","Back"];
-
-    var i=0;
-    for (i;i<=operation.length;i++){
-        if (operation[i] == string){
-            if (language == "en")
-            {
-                return english[i];
-            } else {
-                return swedish[i];
+                    }
+                }
             }
         }
-    }
+    });
+}
+
+function writeData(lang) {
+    // First Page
+    getData("welcome-string",lang,"#welcome");
+    getData("sort",lang,"#sortiment");
+    getData("login",lang,"#login");
+    getData("staff",lang,"#staffOption");
+
+    //Login Page
+    getData("login",lang,"#logintext");
+    getData("back",lang,"#back");
+    getData("login",lang,"#login");
 }
