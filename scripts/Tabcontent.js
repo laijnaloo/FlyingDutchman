6908abf44a95d257bff703996f9417340568d245
Lina Andersson <lina.agneta.andersson@gmail.com>
2017-03-01 12:31:08 +0100

    positioned the plus and minus button and added an animation

--- scripts/Tabcontent.js
+++ scripts/Tabcontent.js
@@ -33,3 +33,48 @@ function changeLanguage(language){
 		document.getElementById("sv").style.opacity = "1";
 	}
 }
+
+function countDrinks(change){
+	var counterNr = countNumber.innerHTML;
+
+	if (change=='-1') {
+		if (counterNr != '0') {
+			countNumber.innerHTML = parseInt(counterNr) - 1;
+			document.getElementById("plus").style.opacity = "1";
+			if (countNumber.innerHTML == '0') {
+				document.getElementById("minus").style.opacity = "0.5";
+			}
+		}
+
+	} else {
+		if (counterNr != '9') {
+			countNumber.innerHTML = parseInt(counterNr) + 1;
+			document.getElementById("minus").style.opacity = "1";
+
+			if (countNumber.innerHTML == '9'){
+				document.getElementById("plus").style.opacity = "0.5";
+			}
+		}
+	}
+}
+
+var loop;
+var degrees = 0;
+function rotation(idName, speed){
+	var image = document.getElementById(idName);
+
+	//set css rotation degree
+	image.style.transform = "rotate("+degrees+"deg)";
+
+	//
+	if(degrees < 180){
+		//calls the function again to make it move slowly around itself
+		loop = setTimeout('rotation(\''+idName+'\','+speed+')',speed);
+		degrees++;
+	} else {
+		degrees = 0;
+	}
+
+	document.getElementById("status").innerHTML = "rotate("+degrees+"deg)";
+
+}
