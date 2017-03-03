ed814180403310d0971452a13b09e26a9e7708a0
annanormark <anna.nor94@gmail.com>
2017-03-03 17:07:53 +0100

    Added to local storage so that the order will be saved if leaving page

--- scripts/undo.js
+++ scripts/undo.js
@@ -7,21 +7,29 @@ var cursor; //if no redo done, should always be the length of the array
 
 window.addEventListener("DOMContentLoaded", init, false);
 
+
 function init(){
-    order = [];
-    cursor = 0;
+    order = JSON.parse(localStorage.getItem("order"));
+    cursor = localStorage.getItem("undoCursor");
+    if(order.length == 0) {
+        order = [];
+        cursor = 0;
+    }
 }
 
 /* Pushes the beers ID onto an Array, moves the cursor to end of array (array.len*/
 function add(ID){
     order.splice(cursor, order.length - cursor);
     cursor = order.push(ID);
+    localStorage.setItem("order", JSON.stringify(order));
+    localStorage.setItem("undoCursor", cursor);
 }
 
 /* if array not empty, move cursor back and return that item */
 function undo(){
     if(order.length > 0){
         cursor--;
+        localStorage.setItem("undoCursor", cursor);
         return order[cursor - 1];
     }
     console.log("No undo available");
@@ -32,8 +40,17 @@ function undo(){
 function redo(){
     if(order.length > cursor){
         cursor++;
+        localStorage.setItem("undoCursor", cursor);
         return order[cursor - 1];
     }
     console.log("No redo available");
     return 0;
+}
+
+/* Should be called after order confirmation */
+function clearOrder(){
+    order = [];
+    cursor = 0;
+    localStorage.setItem("order", JSON.stringify(order));
+    localStorage.setItem("undoCursor", cursor);
 }
