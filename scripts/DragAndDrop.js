6cd2f17bfbebd6cba5036273c333e4d099771e62
TobiasAnderssonq <anderssontobias93@gmail.com>
2017-03-06 20:26:08 +0100

    Added totalPrice to orderPage

--- scripts/DragAndDrop.js
+++ scripts/DragAndDrop.js
@@ -1,6 +1,5 @@
 
-
-
+var totalPrice = 0;
 
 function setDraggable() {
     $(".drinks").draggable({
@@ -20,9 +19,12 @@ function setDraggable() {
             //var order = JSON.parse(localStorage.getItem("order"));
             //order.append()
             //localStorage.setItem("order", "Name: " + ui.draggable.find('p')[0].innerHTML);
-            console.log(ui.draggable.find('p')[0].innerHTML);
-            $(document.getElementById("orderName")).append($(ui.draggable.find('p')[0]).clone());
-            $(document.getElementById("orderPrice")).append($(ui.draggable.find('p')[1]).clone());
+
+            totalPrice=totalPrice+ (parseInt($(ui.draggable.find('p')[1]).text().trim()));
+            document.getElementById("totalPrice").innerHTML = totalPrice;
+
+            $(document.getElementById("orderName")).prepend($(ui.draggable.find('p')[0]).clone());
+            $(document.getElementById("orderPrice")).prepend($(ui.draggable.find('p')[1]).clone());
 
         }
     });
