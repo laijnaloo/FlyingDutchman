db5a635d91375694998d157e9aa0e368a2760f87
annanormark <anna.nor94@gmail.com>
2017-03-16 15:54:11 +0100

    commented countDrinks

--- scripts/Tabcontent.js
+++ scripts/Tabcontent.js
@@ -21,22 +21,25 @@ function openTab(event, title, linkTitle){
 	document.getElementById(linkTitle).style.zIndex = "10";
 }
 
+//Changes the cpunter in the page with specific information about drinks
 function countDrinks(change){
-	var counterNr = countNumber.innerHTML;
-	if (change=='-1') {
-		if (counterNr != '1') {
-			countNumber.innerHTML = parseInt(counterNr) - 1;
-			document.getElementById("plus").style.opacity = "1";
+	var counterNr = countNumber.innerHTML; //Get the current count
+	if (change=='-1') { //If minus is pressed
+		if (counterNr != '1') { //If the count isn't one (the lowest possible count)
+			countNumber.innerHTML = parseInt(counterNr) - 1; //Lower the current count by one
+			document.getElementById("plus").style.opacity = "1"; //Makes sure the plus is clearly visible
 			if (countNumber.innerHTML == '1') {
+				//If count is 1 (lowest possible) Make the minus sign change opacity
 				document.getElementById("minus").style.opacity = "0.5";
 			}
 		}
 	} else {
-		if (counterNr != '9') {
+		if (counterNr != '9') { //if plus is pressed and the current count is lower than 9 (the highest possible)
 			countNumber.innerHTML = parseInt(counterNr) + 1;
-			document.getElementById("minus").style.opacity = "1";
+			document.getElementById("minus").style.opacity = "1"; //make sure the minus sign is visible
 
 			if (countNumber.innerHTML == '9'){
+				//If count is 9 (highest possible) Make the plus sign change opacity
 				document.getElementById("plus").style.opacity = "0.5";
 			}
 		}
